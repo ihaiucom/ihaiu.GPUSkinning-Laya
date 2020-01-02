df9c43a7163589229579d471929e721f26d4216c
zengfeng75 <zengfeng75@gmail.com>
2020-01-02 15:22:58 +0800

    no message

---
+++ Unity3D/GPUSkinning/Assets/GPUSkinning/Scripts/TestRead.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRead : MonoBehaviour
+{
+    public TextAsset textAsset;
+    public GPUSkinningAnimation anim;
+
+    [ContextMenu("Read")]
+    public void Read()
+    {
+
+        byte[] bytes = textAsset.bytes;
+        Debug.Log(bytes.Length);
+        Debug.Log(bytes[0]);
+        Debug.Log(bytes[1]);
+        anim = GPUSkinningAnimation.CreateFromBytes(bytes);
+
+    }
+}
