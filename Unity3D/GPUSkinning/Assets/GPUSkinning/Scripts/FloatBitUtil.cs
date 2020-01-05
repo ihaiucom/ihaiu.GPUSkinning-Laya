3939f3f7d52aabc774402b2b36e2bf782cea467e
zengfeng75 <zengfeng75@gmail.com>
2020-01-05 16:11:19 +0800

    save

---
+++ Unity3D/GPUSkinning/Assets/GPUSkinning/Scripts/FloatBitUtil.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FloatBitUtil
+{
+    public struct Vector4Int
+    {
+        public int x;
+        public int y;
+        public int z;
+        public int w;
+
+    }
+
+    public static Color ToColor(this float value)
+    {
+        int flag = value < 0 ? 1 : 0;
+        float num = Mathf.Abs(value);
+        int numInt = Mathf.FloorToInt(num);
+        float numFloat = num - numInt;
+
+        int numM = numInt / 100;
+        int num1 = numInt % 100;
+
+
+
+        Color c = new Color();
+        c.r = numM / 100f / 255F;
+        c.g = num1 / 100f / 255F;
+        c.b = numFloat;
+        c.a = flag;
+
+        return c;
+    }
+
+
+    public static Vector4Int ToBit(this float value)
+    {
+        int flag = value < 0 ? 1 : 0;
+        float num = Mathf.Abs(value);
+        int numInt = Mathf.FloorToInt(num);
+        float numFloat = num - numInt;
+
+        int numM = numInt / 100;
+        int num1 = numInt % 100;
+
+
+
+        Vector4Int c = new Vector4Int();
+        c.x = Mathf.FloorToInt(numM / 100f * 255);
+        c.y = Mathf.FloorToInt((num1 / 100f * 255);
+        c.z = Mathf.FloorToInt(numFloat * 255);
+        c.w = flag * 255;
+
+        return c;
+    }
+
+}
--- Unity3D/GPUSkinning/Assets/TestTextureGpuValue.cs
+++ Unity3D/GPUSkinning/Assets/TestTextureGpuValue.cs
@@ -6,11 +6,27 @@ using UnityEngine;
 public class TestTextureGpuValue : MonoBehaviour
 {
     public float num = 0;
-    public float colorValue = 0;
+    public int numInt = 0;
+
+    public int numMultiple = 1;
+    public int numInt1 = 0;
+    public float numFloat = 0;
+
+
+
+    public float colorIntMValue = 0;
+    public float colorInt1Value = 0;
+    public float colorFloatValue = 0;
+
+    public int numBitFlag = 0;
+    public int numBitIntM = 0;
+    public int numBitInt1 = 0;
+    public int numBitFloat = 0;
+
     public float gpuValue = 0;
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -20,7 +36,32 @@ public class TestTextureGpuValue : MonoBehaviour
 
     public void Test()
     {
-        colorValue = num / 255f;
-        gpuValue = colorValue * 255;
+        numBitFlag = num < 0 ? 1 : 0;
+        numBitFlag *= 255;
+        float n = Mathf.Abs(num);
+        numInt = Mathf.FloorToInt(n);
+        numFloat = n - numInt;
+        numFloat = (int)(numFloat * 100) / 100.0f;
+
+        numMultiple = numInt / 100;
+        numInt1 = numInt % 100;
+
+        colorIntMValue = numMultiple / 100.0f / 255f;
+        colorInt1Value = numInt1 / 100.0f / 255f;
+        colorFloatValue = numFloat / 255f;
+
+        colorIntMValue = Mathf.Clamp01(colorIntMValue);
+
+
+        numBitIntM = Mathf.FloorToInt(numMultiple / 100f * 255);
+        numBitInt1 = Mathf.FloorToInt(numInt1 / 100f * 255);
+        numBitFloat = Mathf.FloorToInt(numFloat * 255);
+
+
+
+        gpuValue = colorIntMValue * 255 * 100 * 100 + colorInt1Value * 255f * 100f + colorFloatValue * 255f;
+        gpuValue *= numBitFlag > 0 ? -1 : 1;
+
+        //gpuValue = colorValue * 255;
     }
 }
